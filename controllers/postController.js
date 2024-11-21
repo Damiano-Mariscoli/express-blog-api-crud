50c4532f05754b6349d5c479d87277213b8f048c
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-11-21 15:54:57 +0100

    FIX: nuovo metodo per trovare l'index e dare all'item creato un id automatico

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,5 @@
 const postsList = require("../data/posts");
-const functions = require("../functions/functions");
+
 const { post } = require("../routers/posts");
 
 function index(req, res) {
@@ -46,14 +46,16 @@ function destroy(req, res) {
 }
 
 function store(req, res) {
+  let newIndex = postsList[postsList.length - 1].id + 1;
   const newPost = {
-    id: postsList.length + 1,
+    id: newIndex,
     title: req.body.title,
     slug: req.body.slug,
     content: req.body.content,
     image: req.body.image,
     tags: req.body.tags,
   };
+  console.log(newPost);
   postsList.push(newPost);
   res.send("creo un nuovo post");
 }
