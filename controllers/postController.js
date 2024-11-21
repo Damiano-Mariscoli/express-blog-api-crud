59a51b359b96e2af1cfad92a13812043e9eb5013
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-11-21 10:54:56 +0100

    FIX: ES. BONUS

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,35 +1,46 @@
-
 const postsList = require("../data/posts");
 
-
-function index(req, res){
-        res.json(postsList);
-
+function index(req, res) {
+  res.json(postsList);
 }
 
+function show(req, res) {
+  let id = req.params.id;
+  if (isNaN(id)) {
+    id = id.toLowerCase();
+  } else {
+    id = parseInt(id);
+  }
 
-function show(req, res){
-    const id = req.params.id;
-  const post = postsList.find((post) => post.id == id);
+  const post = postsList.find((post) => post.id === id);
+  const slug = postsList.find((slug) => slug.slug === id);
   if (post) {
     res.json(post);
+  } else if (slug) {
+    res.json(slug);
   } else {
-    res.json({ error: "Post not found" });
+    res.sendStatus(404);
   }
-;
 }
 
-function destroy(req, res){
-    const id = req.params.id;
-    const postIndex = postsList.findIndex((post) => post.id == id);
-    if (postIndex !== -1) {
-        postsList.splice(postIndex, 1);
-        res.sendStatus(204);
-    } else {
-        res.sendStatus(404);
-    }
+function destroy(req, res) {
+  let id = req.params.id;
+  if (isNaN(id)) {
+    id = id.toLowerCase();
+  } else {
+    id = parseInt(id);
+  }
+  const postIndex = postsList.findIndex(
+    (post) => post.id === id || post.slug === id
+  );
+  if (postIndex !== -1) {
+    console.log(postIndex)
+    postsList.splice(postIndex, 1);
+    console.log(postsList)
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(404);
+  }
 }
 
-
-
-module.exports = { index , show , destroy }
+module.exports = { index, show, destroy };
